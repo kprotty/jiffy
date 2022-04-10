d1fcc5853c5ec7388e0e5afa6ac9c4a92e9abaca
kprotty <kbutcher6200@gmail.com>
2022-04-10 08:24:30 -0500

    loo_ish breaking

--- benches/unbounded.rs
+++ benches/unbounded.rs
@@ -1,93 +1,204 @@
-// use std::{
-//     sync::atomic::{AtomicBool, Ordering},
-//     thread,
-// };
-//
-// use criterion::{criterion_group, criterion_main, Criterion};
-//
-// const THREADS: usize = 12;
-// const MESSAGES: usize = 12 * 200_000;
-//
-// struct Chan {
-//     thread: thread::Thread,
-//     unparked: AtomicBool,
-// }
-//
-// impl Chan {
-//     fn new() -> Self {
-//         Self {
-//             thread: thread::current(),
-//             unparked: AtomicBool::new(false),
-//         }
-//     }
-//
-//     fn send<V, T>(&self, x: &T, f: impl Fn(&T) -> Result<(), V>) -> Result<(), V> {
-//         f(&x).map(|_| self.unpark())
-//     }
-//
-//     fn recv<V, T>(&self, x: &T, f: impl Fn(&T) -> Option<V>) -> Option<V> {
-//         loop {
-//             match f(x) {
-//                 Some(x) => break Some(x),
-//                 None => {
-//                     while !self.try_unpark() {
-//                         thread::park();
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//
-//     fn try_unpark(&self) -> bool {
-//         self.unparked.swap(false, Ordering::Acquire)
-//     }
-//
-//     fn unpark(&self) {
-//         self.unparked
-//             .fetch_update(Ordering::Release, Ordering::Relaxed, |unparked| {
-//                 if unparked {
-//                     None
-//                 } else {
-//                     Some(true)
-//                 }
-//             })
-//             .map(|_| self.thread.unpark())
-//             .unwrap_or(());
-//     }
-// }
-//
-// fn mpsc_bounded(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("mpsc");
-//     group.sample_size(20);
-//
-//     group.bench_function("loo", |b| {
-//         b.iter(|| {
-//             let (t, r) = loo::mpsc::queue();
-//             let c = Chan::new();
-//
-//             crossbeam::scope(|scope| {
-//                 for _ in 0..THREADS {
-//                     scope.spawn({
-//                         let t = t.clone();
-//                         |_| {
-//                             let t = t;
-//                             for i in 0..MESSAGES / THREADS {
-//                                 c.send(&t, |c| Ok::<_, ()>(c.push_back(i))).unwrap();
-//                             }
-//                         }
-//                     });
-//                 }
-//
-//                 for _ in 0..MESSAGES {
-//                     c.recv(&r, |c| c.pop_front()).unwrap();
-//                 }
-//             })
-//             .unwrap();
-//         })
-//     });
-//
-//     group.finish();
-// }
-//
-// criterion_group!(benches, mpsc_bounded);
-// criterion_main!(benches);
+use std::{
+    sync::atomic::{AtomicBool, Ordering},
+    thread,
+};
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+struct Chan<T> {
+    thread: thread::Thread,
+    unparked: AtomicBool,
+    inner: T,
+}
+
+impl<T> Chan<T> {
+    fn new(inner: T) -> Self {
+        Self {
+            thread: thread::current(),
+            unparked: AtomicBool::new(false),
+            inner,
+        }
+    }
+
+    fn send<V>(&self, f: impl Fn(&T) -> Result<(), V>) -> Result<(), V> {
+        f(&self.inner).map(|_| self.unpark())
+    }
+
+    fn recv<V>(&self, f: impl Fn(&T) -> Option<V>) -> Option<V> {
+        loop {
+            match f(&self.inner) {
+                Some(x) => break Some(x),
+                None => {
+                    while !self.try_unpark() {
+                        thread::park();
+                    }
+                }
+            }
+        }
+    }
+
+    fn try_unpark(&self) -> bool {
+        self.unparked.swap(false, Ordering::Acquire)
+    }
+
+    fn unpark(&self) {
+        self.unparked
+            .fetch_update(Ordering::Release, Ordering::Relaxed, |unparked| {
+                if unparked {
+                    None
+                } else {
+                    Some(true)
+                }
+            })
+            .map(|_| self.thread.unpark())
+            .unwrap_or(());
+    }
+}
+
+fn mpsc(c: &mut Criterion) {
+    let threads = num_cpus::get() - 2;
+    let messages = threads * 200_000;
+
+    let mut group = c.benchmark_group("mpsc");
+
+    group.bench_function("loo_ish", |b| {
+        b.iter(|| {
+            let x = std::sync::Barrier::new(threads + 1);
+            let queue = Chan::new(jiffy::loo_ish::Queue::EMPTY);
+
+            crossbeam::scope(|scope| {
+                for _ in 0..threads {
+                    scope.spawn(|_| {
+                        x.wait();
+                        for i in 0..messages / threads {
+                            queue.send(|c| Result::<(), usize>::Ok(c.send(i))).unwrap();
+                        }
+                    });
+                }
+
+                x.wait();
+                for _ in 0..messages {
+                    queue.recv(|c| unsafe { c.try_recv() }).unwrap();
+                }
+            })
+            .unwrap();
+        })
+    });
+
+    // group.bench_function("jiffy", |b| {
+    //     b.iter(|| {
+    //         let x = std::sync::Barrier::new(threads + 1);
+    //         let queue = Chan::new(jiffy::unbounded::Queue::new());
+
+    //         crossbeam::scope(|scope| {
+    //             for _ in 0..threads {
+    //                 scope.spawn(|_| {
+    //                     x.wait();
+    //                     for i in 0..messages / threads {
+    //                         queue.send(|c| c.push(i)).unwrap();
+    //                     }
+    //                 });
+    //             }
+
+    //             x.wait();
+    //             for _ in 0..messages {
+    //                 queue.recv(|c| c.pop()).unwrap();
+    //             }
+    //         })
+    //         .unwrap();
+    //     })
+    // });
+
+    group.bench_function("crossbeam-queue", |b| {
+        b.iter(|| {
+            let x = std::sync::Barrier::new(threads + 1);
+            let queue = Chan::new(crossbeam::queue::SegQueue::new());
+
+            crossbeam::scope(|scope| {
+                for _ in 0..threads {
+                    scope.spawn(|_| {
+                        x.wait();
+                        for i in 0..messages / threads {
+                            queue.send(|c| Result::<(), usize>::Ok(c.push(i))).unwrap();
+                        }
+                    });
+                }
+
+                x.wait();
+                for _ in 0..messages {
+                    queue.recv(|c| c.pop()).unwrap();
+                }
+            })
+            .unwrap();
+        })
+    });
+
+    group.bench_function("crossbeam-chan", |b| {
+        b.iter(|| {
+            let (tx, rx) = crossbeam::channel::unbounded();
+
+            crossbeam::scope(|scope| {
+                for _ in 0..threads {
+                    scope.spawn(|_| {
+                        for i in 0..messages / threads {
+                            tx.try_send(i).unwrap();
+                        }
+                    });
+                }
+
+                for _ in 0..messages {
+                    rx.recv().unwrap();
+                }
+            })
+            .unwrap();
+        })
+    });
+
+    group.bench_function("std", |b| {
+        b.iter(|| {
+            let (tx, rx) = std::sync::mpsc::channel();
+
+            crossbeam::scope(|scope| {
+                for _ in 0..threads {
+                    let tx = tx.clone();
+                    scope.spawn(move |_| {
+                        for i in 0..messages / threads {
+                            tx.send(i).unwrap();
+                        }
+                    });
+                }
+
+                for _ in 0..messages {
+                    rx.recv().unwrap();
+                }
+            })
+            .unwrap();
+        })
+    });
+
+    group.bench_function("flume", |b| {
+        b.iter(|| {
+            let (tx, rx) = flume::unbounded();
+
+            crossbeam::scope(|scope| {
+                for _ in 0..threads {
+                    scope.spawn(|_| {
+                        for i in 0..messages / threads {
+                            tx.try_send(i).unwrap();
+                        }
+                    });
+                }
+
+                for _ in 0..messages {
+                    rx.recv().unwrap();
+                }
+            })
+            .unwrap();
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, mpsc);
+criterion_main!(benches);
--- src/loo_ish.rs
+++ src/loo_ish.rs
@@ -1,4 +1,4 @@
-use crate::utils::{StrictProvenance, CachePadded, FetchAddPtr};
+use crate::utils::{CachePadded, FetchAddPtr, StrictProvenance};
 use std::{
     ptr::{self, NonNull},
     cell::{Cell, UnsafeCell},
@@ -10,21 +10,13 @@ struct Value<T>(UnsafeCell<MaybeUninit<T>>);
 
 impl<T> Value<T> {
     const EMPTY: Self = Self(UnsafeCell::new(MaybeUninit::uninit()));
-
-    unsafe fn write(&self, value: T) {
-        self.0.get().write(MaybeUninit::new(value))
-    }
-
-    unsafe fn read(&self) -> T {
-        self.0.get().read().assume_init()
-    }
 }
 
+// max sender threads = BLOCK_ALIGN - BLOCK_SIZE - 1
+// thats how much the fetch_add() in push() is allowed to bump until it overflows into the block ptr.
 const BLOCK_ALIGN: usize = 4096;
-const BLOCK_MASK: usize = BLOCK_ALIGN - 1;
+const BLOCK_MASK: usize = !(BLOCK_ALIGN - 1);
 const BLOCK_SIZE: usize = 1024;
-
-// max amount of sender threads == BLOCK_ALIGN
 
 #[repr(align(4096))]
 struct Block<T> {
@@ -34,160 +26,168 @@ struct Block<T> {
 }
 
 impl<T> Block<T> {
-    const NOT_STORED: AtomicBool = AtomicBool::new(false);
+    const UNSTORED: AtomicBool = AtomicBool::new(false);
     const EMPTY: Self = Self {
         values: [Value::<T>::EMPTY; BLOCK_SIZE],
-        stored: [Self::NOT_STORED; BLOCK_SIZE],
+        stored: [Self::UNSTORED; BLOCK_SIZE],
         next: AtomicPtr::new(ptr::null_mut()),
     };
 }
 
-pub struct Queue<T> {
-    head: CachePadded<AtomicPtr<Block<T>>>,
-    tail: CachePadded<AtomicPtr<Block<T>>>,
-    next: CachePadded<AtomicPtr<Block<T>>>,
+struct Producer<T> {
+    block_and_index: AtomicPtr<Block<T>>,
 }
 
+struct Consumer<T> {
+    block: AtomicPtr<Block<T>>,
+    index: Cell<usize>,
+}
+
+pub struct Queue<T> {
+    producer: CachePadded<Producer<T>>,
+    consumer: CachePadded<Consumer<T>>,
+}
+
+unsafe impl<T: Send> Send for Queue<T> {}
+unsafe impl<T: Send> Sync for Queue<T> {}
+
 impl<T> Queue<T> {
-    pub const fn new() -> Self {
-        Self {
-            head: CachePadded(AtomicPtr::new(ptr::null_mut())),
-            tail: CachePadded(AtomicPtr::new(ptr::null_mut())),
-            next: CachePadded(AtomicPtr::new(ptr::null_mut())),
-        }
-    }
+    pub const EMPTY: Self = Self {
+        producer: CachePadded(Producer {
+            block_and_index: AtomicPtr::new(ptr::null_mut()),
+        }),
+        consumer: CachePadded(Consumer {
+            block: AtomicPtr::new(ptr::null_mut()),
+            index: Cell::new(0),
+        }),
+    };
 
     pub fn send(&self, value: T) {
-        let mut next_block = ptr::null_mut::<Block<T>>();
+        let mut allocated_block = ptr::null_mut::<Block<T>>();
 
-        loop {
-            let mut tail = self.tail.fetch_add(1, Ordering::Acquire);
-            let block = tail.map_addr(|addr| addr & !BLOCK_MASK);
-            let index = tail.addr() & BLOCK_MASK;
+        let (block, index, prev_block_next) = 'reserved: loop {
+            let mut block_and_index = self.producer.block_and_index.fetch_add(1, Ordering::Acquire);
+            let block = block_and_index.map_addr(|addr| addr & BLOCK_MASK);
+            let index = block_and_index.addr() & !BLOCK_MASK;
 
             if !block.is_null() && index < BLOCK_SIZE {
-                unsafe {
-                    if !next_block.is_null() {
-                        drop(Box::from_raw(next_block));
-                    }
-
-                    (*block).values.get_unchecked(index).write(value);
-                    (*block).stored.get_unchecked(index).store(true, Ordering::Release);
-                    return;
-                }
+                assert_ne!(index, 0);
+                break 'reserved (block, index, None);
             }
 
-            let mut next = self.next.load(Ordering::Acquire);
-            if ptr::eq(next, block) {
-                if next_block.is_null() {
-                    next_block = Box::into_raw(Box::new(Block::EMPTY));
-                }
-                
-                let new_next = next_block;
-                next = match self.next.compare_exchange(
-                    next,
-                    new_next,
-                    Ordering::AcqRel,
-                    Ordering::Acquire,
-                ) {
-                    Err(e) => e,
-                    Ok(_) => {
-                        next_block = ptr::null_mut();
-                        new_next
-                    }
-                };
+            if allocated_block.is_null() {
+                allocated_block = Box::into_raw(Box::new(Block::EMPTY));
             }
 
-            tail = self.tail.load(Ordering::Relaxed);
             loop {
-                let current_block = tail.map_addr(|addr| addr & !BLOCK_MASK);
-                if !ptr::eq(block, current_block) {
+                let block = block_and_index.map_addr(|addr| addr & BLOCK_MASK);
+                assert!(!ptr::eq(block, allocated_block), "this is apparently hit?");
+
+                let index = block_and_index.addr() & !BLOCK_MASK;
+                if !block.is_null() && index < BLOCK_SIZE {
                     break;
                 }
 
-                if let Err(e) = self.tail.compare_exchange_weak(
-                    tail,
-                    next.map_addr(|addr| addr | 1),
-                    Ordering::Release,
+                if let Err(e) = self.producer.block_and_index.compare_exchange_weak(
+                    block_and_index,
+                    allocated_block.map_addr(|addr| addr | 1),
+                    Ordering::AcqRel,
                     Ordering::Relaxed,
                 ) {
-                    tail = e;
+                    block_and_index = e;
                     continue;
                 }
 
-                unsafe {
-                    let prev_block_next = match NonNull::new(block) {
-                        Some(prev_block) => NonNull::from(&prev_block.as_ref().next),
-                        None => NonNull::from(&*self.head),
-                    };
+                let prev_block_next = match NonNull::new(block) {
+                    Some(prev_block) => NonNull::from(unsafe { &(*block).next }),
+                    None => NonNull::from(&self.consumer.block),
+                };
 
-                    (*next).values.get_unchecked(0).write(value);
-                    (*next).stored.get_unchecked(0).store(true, Ordering::Release);
+                break 'reserved (allocated_block, 0, Some(prev_block_next));
+            }
+        };
 
-                    prev_block_next.as_ref().store(next, Ordering::Release);
-                    return;
-                }
+        unsafe {
+            assert!(index < BLOCK_SIZE);
+            let slot = (*block).values.get_unchecked(index);
+            slot.0.get().write(MaybeUninit::new(value));
+
+            let stored = (*block).stored.get_unchecked(index);
+            assert!(!stored.load(Ordering::Relaxed));
+            stored.store(true, Ordering::Release);
+
+            if let Some(prev_block_next) = prev_block_next {
+                assert!(prev_block_next.as_ref().load(Ordering::Relaxed).is_null());
+                prev_block_next.as_ref().store(block, Ordering::Release);
+            }
+
+            if !allocated_block.is_null() && !ptr::eq(block, allocated_block) {
+                drop(Box::from_raw(allocated_block));
             }
         }
     }
 
     pub unsafe fn try_recv(&self) -> Option<T> {
-        let head = self.head.load(Ordering::Acquire);
-        
-        let mut block = head.map_addr(|addr| addr & !BLOCK_MASK);
+        let mut block = self.consumer.block.load(Ordering::Acquire);
         if block.is_null() {
             return None;
         }
 
-        let mut index = head.addr() & BLOCK_MASK;
+        let mut index = self.consumer.index.get();
         if index == BLOCK_SIZE {
-            block = (*block).next.load(Ordering::Acquire);
-            if block.is_null() {
+            let next_block = (*block).next.load(Ordering::Acquire);
+            if next_block.is_null() {
                 return None;
             }
 
+            assert!({
+                let block_and_index = self.producer.block_and_index.load(Ordering::Relaxed);
+                let producer_block = block_and_index.map_addr(|addr| addr & BLOCK_MASK);
+                !ptr::eq(block, producer_block)
+            });
+
+            drop(Box::from_raw(block));
+            block = next_block;
+            self.consumer.block.store(next_block, Ordering::Relaxed);
+
             index = 0;
-            self.head.store(block, Ordering::Relaxed);
+            self.consumer.index.set(0);
         }
-        
-        if !(*block).stored.get_unchecked(index).load(Ordering::Acquire) {
+
+        assert!(!block.is_null());
+        assert!(index < BLOCK_SIZE);
+
+        let stored = (*block).stored.get_unchecked(index);
+        if !stored.load(Ordering::Acquire) {
             return None;
         }
 
-        let new_head = block.map_addr(|addr| addr | (index + 1));
-        self.head.store(new_head, Ordering::Relaxed);
-
-        let value = (*block).values.get_unchecked(index).read();
-        Some(value)
+        let slot = (*block).values.get_unchecked(index);
+        self.consumer.index.set(index + 1);
+        Some(slot.0.get().read().assume_init())
     }
 }
 
 impl<T> Drop for Queue<T> {
     fn drop(&mut self) {
         unsafe {
-            let head = self.head.load(Ordering::Acquire);
-            let mut block = head.map_addr(|addr| addr & !BLOCK_MASK);
-            let mut index = head.addr() & BLOCK_MASK;
-
-            while !block.is_null() {
-                if !(*block).stored.get_unchecked(index).load(Ordering::Acquire) {
-                    break;
-                }
-
-                drop((*block).values.get_unchecked(index).read());
-                index = (index + 1) % BLOCK_SIZE;
-
-                if index == 0 {
-                    let next_block = (*block).next.load(Ordering::Acquire);
-                    drop(Box::from_raw(block));
-                    block = next_block;
-                }
+            while let Some(value) = self.try_recv() {
+                drop(value);
             }
 
-            if !block.is_null() {
-                assert!((*block).next.load(Ordering::Relaxed).is_null());
-                drop(Box::from_raw(block));
+            let block = self.consumer.block.load(Ordering::Relaxed);
+            if block.is_null() {
+                return;
             }
+
+            assert!({
+                let block_and_index = self.producer.block_and_index.load(Ordering::Relaxed);
+                let producer_block = block_and_index.map_addr(|addr| addr & BLOCK_MASK);
+                ptr::eq(block, producer_block)
+            });
+
+            assert!((*block).next.load(Ordering::Relaxed).is_null());
+            drop(Box::from_raw(block));
         }
     }
 }
